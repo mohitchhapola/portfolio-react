9918259528a7c72c22c874de9c676608200799ea
Mohit Kumar <mohit.sircl@gmail.com>
2023-10-30 12:27:41 +0530

    change in appeare

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,10 @@ function Header() {
   return (
     <>
    <div id='app'>
-   <section className="cyberpunk"> 
-      <a href="https://www.instagram.com/chhapola_mohit/?hl=en" className="cyberpunk2077 scannedh">INSTAGRAM</a>
-      <a href='https://github.com/mohitchhapola' className="cyberpunk2077 green">Git_Hub</a>
-      <a href='https://www.linkedin.com/in/mohit-kumar-062144234/' className="cyberpunk2077 purple">Linked_IN</a>
-      <a href='https://twitter.com/mohitchhapola__' className="cyberpunk2077 blue">Twitter X</a>
-    </section>
+    <section className="cyberpunk ">
+      <div className="center">
+      <img className="cyberpunk dotted" width={"245px"} src={mohit} alt="" />
+      </div>
     <section className="cyberpunk black">
     <h1 className="cyberpunk glitched center">AAGYE TUM!!</h1>
       <h1 className="cyberpunk glitched">Mohit Chhapola, ULTIMATE FULL_STACK BEROZGAAR 😏😏 </h1> 
@@ -19,12 +17,14 @@ function Header() {
       <h2 className="cyberpunk glitched">MERN , DJANGO , PYTHON , RESTapi</h2> 
       <h3 className="cyberpunk">JUST SHAKE YOUR JUICER</h3> 
     </section>
-    <section className="cyberpunk ">
-      <div className="center">
-      <img className="cyberpunk dotted" width={"245px"} src={mohit} alt="" />
-      </div>
       <h2 className='cyberpunk glitched'>RECENTLY LEARNING, SOON WILL APPEAR ON JOB </h2>
       
+    </section>
+   <section className="cyberpunk"> 
+      <a href="https://www.instagram.com/chhapola_mohit/?hl=en" className="cyberpunk2077 scannedh">INSTAGRAM</a>
+      <a href='https://github.com/mohitchhapola' className="cyberpunk2077 green">Git_Hub</a>
+      <a href='https://www.linkedin.com/in/mohit-kumar-062144234/' className="cyberpunk2077 purple">Linked_IN</a>
+      <a href='https://twitter.com/mohitchhapola__' className="cyberpunk2077 blue">Twitter X</a>
     </section>
    </div>
     </>
